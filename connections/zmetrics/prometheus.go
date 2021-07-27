71ac4e15a95a2f1b43e092982609fc08cc98443c
Ezequiel Raynaudo <raynaudo.ee@gmail.com>
2021-07-27 14:54:26 -0300

    Add prometheus wrapper

---
+++ connections/zmetrics/prometheus.go
@@ -0,0 +1,79 @@
+package zmetrics
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
+	"net/http"
+)
+
+type PrometheusCollector prometheus.Collector
+type CounterOpts prometheus.CounterOpts
+type GaugeOpts prometheus.GaugeOpts
+type HistogramOpts prometheus.HistogramOpts
+
+type Counter prometheus.Counter
+type CounterVec *prometheus.CounterVec
+type Gauge prometheus.Gauge
+type Histogram prometheus.Histogram
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func newResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{w, http.StatusOK}
+}
+
+func prometheusMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rw := newResponseWriter(w)
+		next.ServeHTTP(rw, r)
+	})
+}
+
+// StartMetricsServer starts a prometheus server.
+// Data Url is at localhost:<port>/metrics/<endpoint>
+func StartMetricsServer(port string, endpoint string) chan error {
+	router := mux.NewRouter()
+	router.Use(prometheusMiddleware)
+
+	// Prometheus endpoint
+	router.Path("/metrics/" + endpoint).Handler(promhttp.Handler())
+	errChan := make(chan error)
+
+	go func() {
+		err := http.ListenAndServe(fmt.Sprintf(":%s", port), router)
+		if err != nil {
+			zap.S().Errorf("Prometheus server error: %v", err)
+			errChan <- err
+		} else {
+			zap.S().Infof("Prometheus server serving at port %s", port)
+		}
+	}()
+
+	return errChan
+}
+
+func RegisterMetric(c PrometheusCollector) error {
+	return prometheus.Register(c)
+}
+
+func NewSimpleCounter(opts CounterOpts) Counter {
+	return prometheus.NewCounter(prometheus.CounterOpts(opts))
+}
+
+func NewVecCounter(opts CounterOpts, labels []string) CounterVec {
+	return prometheus.NewCounterVec(prometheus.CounterOpts(opts), labels)
+}
+
+func NewGauge(opts GaugeOpts) Gauge {
+	return prometheus.NewGauge(prometheus.GaugeOpts(opts))
+}
+
+func NewHistogram(opts HistogramOpts) prometheus.Histogram {
+	return prometheus.NewHistogram(prometheus.HistogramOpts(opts))
+}
--- go.mod
+++ go.mod
@@ -6,8 +6,10 @@ require (
 	github.com/ThreeDotsLabs/watermill v1.1.1
 	github.com/coinbase/rosetta-sdk-go v0.6.10
 	github.com/eapache/queue v1.1.0
+	github.com/gorilla/mux v1.8.0
 	github.com/hasura/go-graphql-client v0.2.0
 	github.com/minio/minio-go/v7 v7.0.11
+	github.com/prometheus/client_golang v1.11.0
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/viper v1.7.1
 	go.uber.org/zap v1.16.0
