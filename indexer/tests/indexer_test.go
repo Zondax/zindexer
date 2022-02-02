e0780c1939a9a09dffd84ff25b5f1f10387ce1c4
Ezequiel Raynaudo <raynaudo.ee@gmail.com>
2022-02-02 13:23:56 -0300

    Clean up db before each test

--- indexer/tests/indexer_test.go
+++ indexer/tests/indexer_test.go
@@ -1,31 +1,53 @@
 package tests
 
 import (
+	"fmt"
 	"github.com/Zondax/zindexer"
+	"github.com/Zondax/zindexer/components/tracker"
 	"github.com/Zondax/zindexer/indexer/tests/utils"
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
 	"os"
 	"testing"
 )
 
 func TestMain(m *testing.M) {
+	viper.SetDefault("db_schema", "testing")
 	zindexer.InitGlobalLogger()
 	c := m.Run()
 	os.Exit(c)
 }
 
+func setupTestingDB(db *gorm.DB) {
+	err := db.Transaction(func(sqlTx *gorm.DB) error {
+		sqlTx.Exec(fmt.Sprintf("DROP TABLE IF EXISTS %s", tracker.DbSection{}.TableName()))
+		sqlTx.Exec(fmt.Sprintf("DROP TABLE IF EXISTS %s", DummyBlock{}.TableName()))
+		err := sqlTx.AutoMigrate(tracker.DbSection{}, DummyBlock{})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
 func Test_BasicIndexer(t *testing.T) {
 	dbConn := utils.InitdbConn()
+	setupTestingDB(dbConn)
 	err := dbConn.AutoMigrate(DummyBlock{})
 	if err != nil {
 		panic(err)
 	}
 
+	// Create the indexer
 	zidx := NewMockIndexer(dbConn, "test")
 
 	// Set the cb function that will be called when a buffer's sync event triggers
 	zidx.BaseIndexer.SetSyncCB(zidx.MockSyncToDB)
 
-	// Set up the workers
+	// Set up workers
 	zidx.BaseIndexer.SetWorkerConstructor(zidx.NewMockWorker)
 	zidx.BaseIndexer.BuildWorkers(10)
 
