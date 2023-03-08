3d11cce3b57259bf2ab28735f279e1c20f0cb23a
Ezequiel Raynaudo <raynaudo.ee@gmail.com>
2023-03-08 12:45:20 -0300

    Add listChan function (#43)

--- components/connections/data_store/interface.go
+++ components/connections/data_store/interface.go
@@ -1,10 +1,13 @@
 package data_store
 
+import "context"
+
 type IDataStoreClient interface {
 	GetFile(name string, location string) ([]byte, error)
 	UploadFromFile(filePath string, dest string) error
 	UploadFromBytes(data []byte, destFolder string, destName string) error
 	List(dir string, prefix string) ([]string, error)
+	ListChan(ctx context.Context, dir string, prefix string) (<-chan string, error)
 	StorageType() string
 }
 
